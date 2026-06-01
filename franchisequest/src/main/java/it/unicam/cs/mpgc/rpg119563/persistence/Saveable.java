f298ea099b79c2f2e2dad710708424e09d67d306
Lou-Pomannar6996 <alessandro.polenta@studenti.unicam.it>
2026-06-02 00:29:42 +0200

    removed savable.java for avoid a denied dependences

--- franchisequest/src/main/java/it/unicam/cs/mpgc/rpg119563/persistence/Saveable.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.mpgc.rpg119563.persistence;
-
-/**
- * Contratto per oggetti che possono essere serializzati/deserializzati in XML.
- */
-public interface Saveable {
-    void save(XmlSaveManager manager);
-    void load(XmlLoadManager manager);
-}
